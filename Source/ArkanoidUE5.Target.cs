ed6b67139d1124f16539d2d01c530c02bb7f1455
Pavel Kral <frostxx@tiscali.cz>
2025-05-17 10:00:05 +0200

    log#

--- Source/ArkanoidUE5.Target.cs
+++ Source/ArkanoidUE5.Target.cs
@@ -5,11 +5,11 @@ using System.Collections.Generic;
 
 public class ArkanoidUE5Target : TargetRules
 {
-	public ArkanoidUE5Target( TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		  DefaultBuildSettings = BuildSettingsVersion.V5;
-		  IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+     public ArkanoidUE5Target(TargetInfo Target) : base(Target)
+     {
+          Type = TargetType.Game;
+          DefaultBuildSettings = BuildSettingsVersion.V5;
+          IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
           ExtraModuleNames.Add("ArkanoidUE5");
-	}
+     }
 }
