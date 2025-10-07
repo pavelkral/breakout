2ed12b687a4143cde0eae675250256a39adb6f03
Pavel Kral <frostxx@tiscali.cz>
2025-10-07 13:29:24 +0200

    Update project to Unreal Engine 5.6
    Upgraded EngineAssociation to 5.6 in the uproject file and updated include order versions in target files to Unreal5_6. Also updated .vsconfig to add Unreal and Clang components for improved development support.

--- Source/ArkanoidUE5.Target.cs
+++ Source/ArkanoidUE5.Target.cs
@@ -9,7 +9,7 @@ public class ArkanoidUE5Target : TargetRules
      {
           Type = TargetType.Game;
           DefaultBuildSettings = BuildSettingsVersion.V5;
-          IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+          IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
           ExtraModuleNames.Add("ArkanoidUE5");
      }
 }
